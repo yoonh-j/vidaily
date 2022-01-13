b64d3dd473c76971abe7c27303aa5d609d160e30
yoonh-j <akopysj177@naver.com>
2022-01-13 18:57:56 +0900

    AWS Amplify, Cognito 추가

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "vidailyed5d8ded": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
